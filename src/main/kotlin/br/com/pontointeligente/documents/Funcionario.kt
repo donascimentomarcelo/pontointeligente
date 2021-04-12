3fa0e930ccb6f69059683acae9179579769518f6
Marcelo <marcelojunin2010@hotmail.com>
2021-04-11 21:29:27 -0300

    creating entities and enums

---
+++ src/main/kotlin/br/com/pontointeligente/documents/Funcionario.kt
@@ -0,0 +1,20 @@
+package br.com.pontointeligente.documents
+
+import br.com.pontointeligente.enums.PerfilEnum
+import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.mapping.Document
+
+@Document
+data class Funcionario (
+        val nome: String,
+        val email: String,
+        val senha: String,
+        val cpf: String,
+        val perfil: PerfilEnum,
+        val empresaId: String,
+        val valorHora: Double? = 0.0,
+        val qtdHorasTrabalhoDia: Float? = 0.0f,
+        val qtdHorasAlmoco: Float? = 0.0f,
+        @Id
+        val id: String? = null,
+)
---
+++ src/main/kotlin/br/com/pontointeligente/enums/PerfilEnum.kt
@@ -0,0 +1,6 @@
+package br.com.pontointeligente.enums
+
+enum class PerfilEnum {
+    ROLE_ADMIN,
+    ROLE_USUARIO
+}
